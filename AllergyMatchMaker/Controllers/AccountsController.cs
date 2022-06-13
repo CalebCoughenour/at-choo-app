88db666aa846835c1c83f8dec8ca47cdcc554f11
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-06-13 13:49:16 -0700

    Move  DTO file  and update controllers
    Co-authored-by: Caleb Coughenour <CalebCoughenour@users.noreply.github.com>
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>
    Co-authored-by: Jake Edgar <jakeedgar@users.noreply.github.com>
    o-authored-by: Evgeniya Meshuris <em@theemm.com>

--- AllergyMatchMaker/Controllers/AccountsController.cs
+++ AllergyMatchMaker/Controllers/AccountsController.cs
@@ -1,62 +1,62 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.AspNetCore.Identity;
-// using AllergyMatchMaker.Models;
-// using AllergyMatchMaker.DTO;
-// using System.Threading.Tasks;
-// using System.Net;
-// using System;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using AllergyMatchMaker.Models;
+using AllergyMatchMaker.DTO;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.Linq;
 
-// namespace AllergyMatchMaker.Controllers
-// {
-//   [Route("api/[controller]")]
-//   [ApiController]
-//   public class AccountsController : ControllerBase
-//   {
-//     private readonly UserManager<ApplicationUser> _userManager;
-//     private readonly SignInManager<ApplicationUser> _signInManager;
+namespace AllergyMatchMaker.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AccountsController : ControllerBase
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
 
-//     public AccountsController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
-//     {
-//       _userManager = userManager;
-//       _signInManager = signInManager;
-//     }
+    public AccountsController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+    }
 
-//     [HttpPost("register")]
-//     public async Task<ActionResult<RegisterUserDTO>> Register (RegisterUserDTO model)
-//     {
-//       Console.WriteLine("HIT");
-//       var user = new ApplicationUser { UserName = model.Email };
-//       IdentityResult result = await _userManager.CreateAsync(user, model.Password);
-//       if (result.Succeeded)
-//       {
-//         return Ok(user.Id);
-//       }
-//       else
-//       {
-//         return BadRequest(result.Errors);
-//       }
-//     }
-//     [HttpGet]
-//     public string GetString ()
-//     {
-//       return "works";
-//     }
+    [HttpPost("register")]
+    public async Task<ActionResult<RegisterUserDTO>> Register (RegisterUserDTO model)
+    {
+      Console.WriteLine("HIT");
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return Ok(user.Id);
+      }
+      else
+      {
+        return BadRequest(result.Errors);
+      }
+    }
+    [HttpGet]
+    public string GetString ()
+    {
+      return "works";
+    }
 
-//     [HttpPost("Login")]
-//     public async Task<ActionResult> Login(LoginUserDTO model)
-//     {
-//       Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+    [HttpPost("Login")]
+    public async Task<ActionResult> Login(LoginUserDTO model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
       
-//       if (result.Succeeded)
-//       {
-//         var user = await _userManager.FindByNameAsync(model.Email);
-//           return Ok(user.Id);
-//       }
-//       else
-//       {
-//           return BadRequest();
-//       }
-//     }
-//   }
-// }
+      if (result.Succeeded)
+      {
+        var user = await _userManager.FindByNameAsync(model.Email);
+          return Ok(user.Id);
+      }
+      else
+      {
+          return BadRequest();
+      }
+    }
+  }
+}
--- AllergyMatchMaker/Startup.cs
+++ AllergyMatchMaker/Startup.cs
@@ -8,6 +8,7 @@ using AllergyMatchMaker.Models;
 using System;
 using System.Reflection;
 using System.IO;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
 
 namespace AllergyMatchMaker.Solution
@@ -27,6 +28,20 @@ namespace AllergyMatchMaker.Solution
           services.AddDbContext<AllergyMatchMakerContext>(opt =>
               opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
           services.AddControllers();
+          
+          services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AllergyMatchMakerContext>()
+            .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 0;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 0;
+            });
           //Swagger
           services.AddSwaggerGen(c=>
           {
